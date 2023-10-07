f732f67df234488a03c95b1e8a80927cb3d577f2
Vladimir Dementyev <palkan@hey.com>
2023-10-07 21:23:14 +0200

    turbo morph drive

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @7.3.0
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @7.3.0
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.0
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "idiomorph" # https://github.com/basecamp/idiomorph/blob/rollout-build/dist/idiomorph-esm.js
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "utils/fake_audio", to: "utils/fake_audio.js"
