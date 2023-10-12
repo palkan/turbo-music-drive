6b9418f5ced1803493775a6f09a071732b206d56
Vladimir Dementyev <palkan@hey.com>
2023-10-12 16:47:22 -0700

    fix: no need to override connect() in AnimatedNumber

--- app/javascript/controllers/animated_number_controller.js
+++ app/javascript/controllers/animated_number_controller.js
@@ -1,10 +1,6 @@
 import AnimatedNumber from "stimulus-animated-number";
 
 export default class extends AnimatedNumber {
-  connect() {
-    super.connect();
-  }
-
   endValueChanged(_newValue, oldValue) {
     this.startValue = oldValue;
     this.animate();
